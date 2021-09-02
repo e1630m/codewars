9f3fc0398c3d4086cdd6467b2d8fff6d572befcf
e1630m <e1630m@gmail.com>
2021-09-02 02:31:35 +0000

    Initial Commmit 4kyu/human_readable_duration_format

---
+++ 4kyu/human_readable_duration_format.py
@@ -0,0 +1,14 @@
+def format_duration(n: int) -> str:
+    count = {k: 0 for k in 'year day hour minute second'.split()}
+    count['year'], n = divmod(n, 31536000)
+    count['day'], n = divmod(n, 86400)
+    count['hour'], n = divmod(n, 3600)
+    count['minute'], count['second'] = divmod(n, 60)
+    ans = ''
+    c = {k + 's' if v > 1 else k: v for k, v in count.items() if v}
+    l = len(c)
+    for k, v in c.items():
+        l -= 1
+        separator = ', ' if l > 1 else ' and ' if l else ''
+        ans += f'{v} {k}' + separator
+    return ans if ans else 'now'
